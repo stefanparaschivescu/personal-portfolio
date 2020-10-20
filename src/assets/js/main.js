50ae3565e4bb9a0b05fc5d6a05fb3e50a4bad6b1
stefanparaschivescu <stefparaschivescu@gmail.com>
2020-10-20 19:35:09 +0300

    final adjustments

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
   AOS.init( {
-    // uncomment below for on-scroll animations to played only once
     // once: true  
-  }); // initialize animate on scroll library
+  }); 
 });
 
-// Smooth scroll for links with hashes
+
 $('a.smooth-scroll')
 .click(function(event) {
   if (
@@ -13,16 +12,13 @@ $('a.smooth-scroll')
     && 
     location.hostname == this.hostname
   ) {
-    // Figure out element to scroll to
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
-      // Only prevent default if animation is actually gonna happen
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top
       }, 1000, function() {
-        // Callback after animation
         var $target = $(target);
         $target.focus();
         if ($target.is(":focus")) {
